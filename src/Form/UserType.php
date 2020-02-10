1c4efe626c936052f4db65dd195feb5b64122929
VictorOrtizCotaina <v.ortiz.cf@gmail.com>
2020-02-10 10:35:33 -0500

    Creació dels CRUD de les entitats principals .

---
+++ src/Form/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('email')
+            ->add('name')
+            ->add('surnames')
+            ->add('province')
+            ->add('lang')
+            ->add('avatar')
+            ->add('dateAdd')
+            ->add('active')
+            ->add('idUserGroup')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}

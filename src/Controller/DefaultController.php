974fd6cea943be89935a438fcc9c4a28cb8ee5ec
VictorOrtizCotaina <v.ortiz.cf@gmail.com>
2020-02-13 08:09:00 -0500

    Index.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/", name="homepage")
      */
     public function index()
     {
+        // get the Link repository (it is like our model)
+        $repository = $this->getDoctrine()->getRepository(Category::class);
+
+        // retrieve all links
+        $categories = $repository->findAll();
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'categories' => $categories
         ]);
     }
 }

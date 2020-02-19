4088b2fc8486ff9aabfeb5323819e9354fbbf97a
VictorOrtizCotaina <v.ortiz.cf@gmail.com>
2020-02-19 12:45:39 -0500

    Registro.

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,12 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -23,15 +22,7 @@ class RegistrationFormType extends AbstractType
             ->add('surnames')
             ->add('email')
             ->add('province')
-            ->add('lang')
-            ->add('role', ChoiceType::class,
-                [ 'choices'  => [
-                    'Usuari normal' => 'ROLE_USER',
-                    'Admnistrador' => 'ROLE_ADMIN'
-                ],
-                    'multiple'=>true,
-                    'expanded'=>true,
-                ])
+            ->add('dateAdd', DateType::class, ['widget' => 'single_text'])
             ->add('password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -48,6 +39,14 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ]);
+        /*->add('role', ChoiceType::class,
+        [ 'choices'  => [
+            'Usuari normal' => 'ROLE_USER',
+            'Admnistrador' => 'ROLE_ADMIN'
+        ],
+            'multiple'=>true,
+            'expanded'=>true,
+        ])*/
         /*->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
